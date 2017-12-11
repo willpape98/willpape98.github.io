693bae19f4fe0808257b6f1f22c905cbd2f5114b
(s) William Pape <wpape@pm7-0064.uopnet.plymouth.ac.uk>
2017-12-11 12:01:36 +0000

    final commit

--- sketch.js
+++ sketch.js
@@ -1,48 +1,48 @@
-//https://github.com/stavrosdidakis/DAT-GAD-405_2017
-//https://p5js.org
+//The purpose of this sketch is to provide a basic template
 
-//DAT405 / GAD405
-//10_Exercise 5 - Interactive Color Grid
+//this line of code
+let selection;
 
-let tileSize = 50;
-
+//this is to setup the basics canvas size and framerate
 function setup() {
-  var canvas = createCanvas(500, 500); //Use this function to define the size of the output window
-  stroke(255);
-  canvas.position(500, 200);
 
-  canvas.parent("myContainer");
-  var text = createDiv('This is a HTML string!');
-  text.position(50,50);
+  var canvas = createCanvas(841,594);
+  background(22);
+  canvas.parent ("canvascontainer");
+  //this changes the speed in which the shapes appear on screen
+  frameRate(60);
+}
+//here we have it so each shape has a random colour each time
+  function draw() {
+
+  push();
+
+  fill(random(255), random(255), random(255));
+
+  //here a random width and height is chosen so that the objects appear in random parts of the canvas and dont all start at the same position.
+  translate(random(width), random(height));
+
+
+  //here we a making it so a random number is chosen either 0 1 or 2 and then dependant on the number chosen that it what number will be drawn on the Canvas
+  selection = round(random(2));
+  if (selection===0) ellipse(0, 0, 40, 40);
+  if (selection===1) rect(0, 0, 40, 40);
+  if (selection===2) triangle(30, 75, 57, 20, 81, 75);
+
+  //this logs in console in what order what selection has been made
+  //console.log(selection);
+
+
+  pop();
+
+
+}
+// here we have set it so when left click is pressed the canvas is cleared
+function mousePressed() {
+  clear();
+ background(22);
 }
 
-function draw() {
-  background(0); //Set your background color to black (0)
 
-  //These 3 variables hold the mapped values from the mouse position (from coordinates to 16bit)
-  var mouse1 = map(mouseY, 0, height, 0, 255);
-  var mouse2 = map(mouseX, 0, width, 0, 255);
-  var mouse3 = map(mouseY, 0, width, 255, 0);
 
-  //Four different values are created. The alpha channel is controlled
-  //by the mouse variables we created before
-  var color1 = color(25, 255, 255);
-  var color2 = color(255, 194, 203, mouse1);
-  var color3 = color(36, 255, 23, mouse2);
-  var color4 = color(239, 10, 46, mouse3);
-
-  //The nested for-loop creates the grid, and according to the position of the mouse,
-  //the fill color of the shape will be different
-  for (let y = 0; y < height; y+=tileSize) {
-    for (let x = 0; x < width; x+=tileSize) {
-      //Create a conditional statement that it will check the coordinates of the mouse
-      //in relation to the position of the shapes on the grid, and according to this
-      //a color value will be selected
-      if(mouseX>x && mouseX<x+tileSize && mouseY>y && mouseY<y+tileSize) fill(color1);
-      else if (mouseX>x && mouseX<x+tileSize) fill(color2);
-      else if (mouseY>y && mouseY<y+tileSize) fill(color3);
-      else fill(color4);
-      rect(x, y, tileSize, tileSize);
-    }
-  }
-}
+//reset canvas on button click
